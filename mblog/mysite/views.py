c6605b1d5f5dd74da11454038c4fa6554d265dc8
jane-8239 <1061238239@nkust.edu.com>
2020-11-03 12:03:33 +0800

    first commit

---
+++ mblog/mysite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def homepage(request):
+    lottos = list()
+    for i in range(6):
+        lottos.append(random.randint(1 ,42))
+    
+    return render(request, "index.html", locals())
+     
+
+
